6384a8bd5ce0ee3017c76ee0343d11d7c078df56
Arkadiusz Mincberger <arkadiusz.mincberger@medius.com>
2025-12-29 22:57:27 +0100

    changed to easier expenses

--- lib/features/group/expenses/ExpensesView.dart
+++ lib/features/group/expenses/ExpensesView.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -6,6 +7,7 @@ import 'package:niezapominapka/features/group/expenses/ExpenseListItem.dart';
 import 'package:niezapominapka/features/group/expenses/model/group_expenses_repository.dart';
 
 import '../../groups/model/group_model.dart';
+import 'model/expense_model.dart';
 import 'model/group_expenses_provider.dart';
 
 class ExpensesView extends ConsumerWidget{
@@ -18,13 +20,19 @@ class ExpensesView extends ConsumerWidget{
     final expensesAsync = ref.watch(expensesForGroupProvider(group.id!));
 
     return expensesAsync.when(data: (expenses) {
+          final groupExpenses = groupBy(expenses,
+                  (Expense e) => DateTime(e.date.year, e.date.month, e.date.day));
+
+          final groupedExpensesKeys = groupExpenses.keys.toList();
+          groupedExpensesKeys.sort((a, b) => b.compareTo(a));
+          
           return ListView.separated(
               itemBuilder: (context, index){
-                final e = expenses[index];
-                    return ExpensesForDateListItem(expense: e);
+                final key = groupedExpensesKeys[index];
+                    return ExpensesForDateListItem(expenses: groupExpenses[key]!, date: key,);
               },
               separatorBuilder: (_, __) => const SizedBox(height: 8,),
-              itemCount: expenses.length);
+              itemCount: groupedExpensesKeys.length);
        },
         error: (e, st) => Center(child: Text("Błąd: $e")),
         loading: () => const Center(child: CircularProgressIndicator())
