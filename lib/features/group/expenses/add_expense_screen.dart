f7f2eab5973e4ca4509e938f3cc2d96c83a9f83e
Arkadiusz Mincberger <arkadiusz.mincberger@medius.com>
2025-12-30 19:22:07 +0100

    sth done

--- lib/features/group/expenses/add_expense_screen.dart
+++ lib/features/group/expenses/add_expense_screen.dart
@@ -1,5 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:niezapominapka/components/molecules/AppPage.dart';
+import 'package:niezapominapka/components/molecules/AppTitle.dart';
+import 'package:niezapominapka/components/oprganisms/number_field.dart';
+import 'package:niezapominapka/features/auth/app_user.dart';
+import 'package:niezapominapka/features/group/expenses/providers/group_users_provider.dart';
 
 import '../../groups/model/group_model.dart';
 
@@ -16,11 +23,80 @@ class AddExpenseScreen extends ConsumerStatefulWidget {
 }
 
 class _AddExpenseScreenState extends ConsumerState<AddExpenseScreen> {
+  final TextEditingController _nameController;
+  final TextEditingController _amountController;
+  String? _whoPayed = null;
+  List<AppUser> _whoToCountIn = [];
+
+  void changeWhoPayed(String value){
+    setState(() => _whoPayed = value);
+  }
+
+  void changeWhoToCountIn(List<AppUser> users){
+    setState(() => _whoToCountIn = users);
+  }
+
+
+
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    final usersForGroup = ref.watch(usersForGroupProvider(widget.group.id!));
+
+    return Scaffold(
+      appBar: AppTitle(showBack: widget.showBack),
+      body: AppPage(child:
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center, //nwm czy to dobra jest xd
+          spacing: 10,
+          children: [
+            Text("Dodaj rozliczenie"),
+            Row(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Nazwa"),
+                    TextField(
+                      controller: _nameController,
+                    )
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Kwota"),
+                    NumberField(
+                      controller: _nameController,
+                    )
+                  ],
+                )
+              ],
+            ),
+            Text("Kto płacił"),
+            usersForGroup.when(
+                data: (users) =>
+                    DropdownField(
+                      value: _whoPayed,
+                      onChange: changeWhoPayed,
+                      availableOptions: users),
+                loading: () => const CircularProgressIndicator(),
+                error: (err, stack) => Text('Błąd: $err'),
+              ),
+            Text("Kogo wliczyć"),
+            usersForGroup.when(
+              data: (users) =>
+                  SelectionList(
+                      value: _whoToCountIn,
+                      onChange: changeWhoToCountIn,
+                      availableOptions: users),
+              loading: () => const CircularProgressIndicator(),
+              error: (err, stack) => Text('Błąd: $err'),
+            ),
+          ],
+        )
+      ),
+    );
   }
 
 }
