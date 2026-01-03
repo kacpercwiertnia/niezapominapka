9ac41096b6c6b9d6f749f9d8d231145c42b07848
Arkadiusz Mincberger <arkadiusz.mincberger@medius.com>
2026-01-03 18:41:46 +0100

    IT SHOULD ALL WORK

--- lib/features/group/shoplist/shop_list_view.dart
+++ lib/features/group/shoplist/shop_list_view.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:niezapominapka/features/group/shoplist/providers/shop_item_group_provider.dart';
+import 'package:niezapominapka/features/group/shoplist/shop_item_card.dart';
 
 import '../../groups/model/group_model.dart';
 
@@ -10,8 +14,33 @@ class ShopListView extends ConsumerWidget{
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // TODO: implement build
-    throw UnimplementedError();
+    final shopItemsAsync = ref.watch(shopItemGroupProvider(group.id!));
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      spacing: 8.0,
+      children: [
+        Text(
+            "Brakujące produkty",
+            style: Theme.of(context).textTheme.titleLarge
+        ),
+        Expanded(
+            child: shopItemsAsync.when(
+                data: (shopItems){
+                  return ListView.separated(
+                      itemBuilder: (context, index) {
+                        final shopItem = shopItems[index];
+                        return ShopItemCard(item: shopItem);
+                      },
+                      separatorBuilder: (_, __) => const SizedBox(height: 8,),
+                      itemCount: shopItems.length);
+                },
+                error: (e, st) => Center(child: Text("Błąd: $e")),
+                loading: () => const Center(child: CircularProgressIndicator())
+            )
+        )
+      ],
+    );
   }
   
 }
